7db6eb935a3e5a80e6c798499a8e3824af36bfff
HystERICKal <nyoroerick12@gmail.com>
2023-07-20 11:41:21 -0700

    I just managed to program my way out of a wet paper bag woo whooo!

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - FizzBuzz interview question
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}  else
+		{
+			printf(" %d", i);
+		}
+		i++;
+	}
+	printf("\n");
+
+
+	return (0);
+}
