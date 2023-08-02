10b55bbb87c49b6583a14bc5d15c2289b5c8e341
HystERICKal <nyoroerick12@gmail.com>
2023-08-02 10:08:16 -0700

    I either don't dream or forget all my dreams

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,14 +4,14 @@
  * @s: string
  */
 void _print_rev_recursion(char *s)
-{       
-        /*If there's still more to the string*/
-        if (*s)
-        {
-                /*Continue till the end*/
-                _print_rev_recursion(s + 1);
-                /*print from the end*/
-                _putchar(*s);
-        }
+{
+	/*If there's still more to the string*/
+	if (*s)
+	{
+		/*Continue till the end*/
+		_print_rev_recursion(s + 1);
+		/*print from the end*/
+		_putchar(*s);
+	}
 }
 
